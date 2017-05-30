df857e941280acdb2cc78590f16d8971b02d621f
Miguel Angel Gonzales Calsin <miguelangelgonzales@viajeseci.es>
2017-05-30 16:56:21 +0200

    Solved issues detected with hystrix related to metrics and other dependencies changes in pom.

--- hello-world/src/main/java/com/viajeseci/example/microservices/Application.java
+++ hello-world/src/main/java/com/viajeseci/example/microservices/Application.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Created by magonzales on 25/05/17.
  */
 //Permite registrar el microservicio en el servidor EUREKA
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- hystrix-monitor/src/main/java/com/viajeseci/example/infraestructure/Application.java
+++ hystrix-monitor/src/main/java/com/viajeseci/example/infraestructure/Application.java
@@ -1,20 +1,17 @@
-package com.viajeseci.example.microservices;
+package com.viajeseci.example.infraestructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Created by magonzales on 26/05/17.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@EnableHystrix
+@EnableHystrixDashboard
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
